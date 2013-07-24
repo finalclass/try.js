3cccb684a2207c27bcc88d9a6f71140e3922a269
Szymon Wygnanski <s@finalclass.net>
2013-07-24 18:46:59 +0200

    -

--- README.md
+++ README.md
@@ -8,7 +8,41 @@ Examples are the best source of documentation - read spec/ directory content to 
 npm install try
 ```
 
-## usage
+## async usage (increment file
+
+```js
+
+var Try = require('try'),
+    fs = require('fs');
+
+new Try
+    (function () {
+        fs.readdir('./spec', this.pause());
+    })
+    (function (err, files) {
+        if (err) { throw err; }
+        var next = this.pause(files.length);
+
+        files.forEach(function (file) {
+            console.log('file', file);
+            fs.readFile('./spec/' + file, next);
+        });
+    })
+    (function () {
+        return Array.prototype.map.call(arguments, function (args) {
+            return args[1].toString();
+        }).join('');
+    })
+    (function (result) {
+        dirContent = result;
+    })
+    .catch(function (err) {
+        console.log('catch', err.stack);
+    })
+    .run();
+```
+
+## sync usage
 
 ```js
 var Try = require('try'),
---
+++ spec/it-increment-file-content.spec.js
@@ -0,0 +1,44 @@
+describe('it-increment-file-content.spec', function () {
+
+    var Try = require('../Try.js'),
+        fs = require('fs');
+
+    it('it-increment-file-content.spec', function () {
+        var dirContent = '';
+
+        new Try
+            (function () {
+                fs.readdir('./spec', this.pause());
+            })
+            (function (err, files) {
+                if (err) { throw err; }
+                var next = this.pause(files.length);
+
+                files.forEach(function (file) {
+                    console.log('file', file);
+                    fs.readFile('./spec/' + file, next);
+                });
+            })
+            (function () {
+                return Array.prototype.map.call(arguments, function (args) {
+                    return args[1].toString();
+                }).join('');
+            })
+            (function (result) {
+                dirContent = result;
+            })
+            .catch(function (err) {
+                console.log('catch', err.stack);
+            })
+            .run();
+
+        waitsFor(function () {
+            return dirContent !== '';
+        }, 'read all files', 100);
+
+        runs(function () {
+            expect(dirContent !== '').toBeTruthy();
+        });
+    });
+
+});
