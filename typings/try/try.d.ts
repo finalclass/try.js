948eb7cc8fd2c705535d3c260cc6e74351deef9f
Szymon Wygnanski <s@finalclass.net>
2014-12-01 13:17:59 +0100

    switched to interfaces

--- typings/try/try.d.ts
+++ typings/try/try.d.ts
@@ -1,22 +1,30 @@
 declare module 'try' {
 
-  function tryjs(callback?:{(...args):any}[]) : tryjs.ITry;
-  function tryjs(callback?:(...args)=>any) : tryjs.ITry;
-
-  module tryjs {
-    function pause(n?:number) : (...args) => ITry;
-    function throwFirstArgument(...args) : void;
-    function throwFirstArgumentInArray(...args) : void[];
-
-    interface ITry {
-      (callback?:{(...args):any}[]) : ITry;
-      (callback?:(...args)=>any) : ITry;
-      then(callback:(...args)=>void) : ITry;
-      catch(callback:(err:Error)=>void) : ITry;
-      finally(callback:(...args)=>void) : ITry;
-      run(func?:(...args)=>void, args?:any[]) : ITry;    
-    }
+  interface ICallback {
+    (...args:any[]):any;
   }
 
-  export = tryjs;
+  interface ICallChain {
+    (callbacks?:ICallback[]) : ITry;
+    (callback?:ICallback) : ITry;
+  }
+
+  interface ITry extends ICallChain {
+    then(callback:ICallback) : ITry;
+    catch(callback:(err:Error)=>void) : ITry;
+    finally(callback:ICallback) : ITry;
+    run(func?:ICallback, args?:any[]) : ITry;
+  }
+
+  interface ITryStatic extends ICallChain {
+    new(callback?:ICallback[]):ITry;
+    new(callback?:ICallback):ITry;
+    pause(n?:number):(...args:any[]) => ITry;
+    throwFirstArgument(...args:any[]): void;
+    throwFirstArgumentInArray(...args:any[]): void[];
+  }
+
+  var Try:ITryStatic;
+
+  export = Try;
 }
